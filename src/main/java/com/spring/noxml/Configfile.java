8539d83737249396f97e766499f039005e4ef041
AravindNaani <aravindbujji7@gmail.com>
2024-01-08 16:39:50 +0530

    Added @Bean Configuration

--- src/main/java/com/spring/noxml/Configfile.java
+++ src/main/java/com/spring/noxml/Configfile.java
@@ -1,5 +1,6 @@
 package com.spring.noxml;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,5 +9,15 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan(basePackages = "com.spring.noxml")
 @PropertySource(value = "classpath:application.properties")
 public class Configfile {
+	
+//	@Bean
+//	public Employee getEmployeebean() {
+//		return new Employee();
+//	}
+//	
+//	@Bean
+//	public Address getAddressbean() {
+//		return new Address();
+//	}
 
 }
